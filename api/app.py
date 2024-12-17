5b8048a042e9579487521be354988311e7768ec9
zRyyH <zryyh.br@gmail.com>
2024-12-17 15:01:03 -0300

    UPDATE V2

--- api/app.py
+++ api/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 
+
 app = Flask(__name__)
+CORS(app)
+
 
 # Rota que faz o proxy para a API externa
-@app.route('/api/auth/signin', methods=['POST'])
-def proxy_signin():
+@app.route('/', methods=['POST'])
+def proxyEduardo():
     try:
         # Dados recebidos do frontend
         data = request.json
@@ -26,4 +30,4 @@ def proxy_signin():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=10000)
